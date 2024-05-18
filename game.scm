83ea4d8147260437ae64634cf2298a38aae2aeaa
Max Carvajal <jcarvajal288@gmail.com>
2024-05-18 13:04:54 -0500

    Create level structure with ship

--- game.scm
+++ game.scm
@@ -15,27 +15,43 @@
         (math rect)
         (math vector))
 
+(define-record-type <ship>
+  (make-ship velocity hitbox)
+  ship?
+  (velocity ship-velocity)
+  (hitbox ship-hitbox))
+
+(define-record-type <level>
+  (make-level ship)
+  level?
+  (ship level-ship))
+
 (define image:ship (make-image "assets/images/ship-1.png"))
 
 (define game-width    1280.0)
 (define game-height   1024.0)
 (define ship-width    43.0)
 (define ship-height   41.0)
-(define ship-hitbox (make-rect (- (/ game-width 2.0) (/ ship-width 2)) 
+
+(define (make-level-1)
+  (make-level (make-ship (vec2 0.0 0.0)
+                         (make-rect (- (/ game-width 2.0) (/ ship-width 2)) 
                                (- (/ game-height 2.0) (/ ship-height 2))
                                ship-width 
-                               ship-height))
+                               ship-height))))
+
+(define *level* (make-level-1))
 
 (define (draw-background)
     (set-fill-color! context "#140c1c")
     (fill-rect context 0.0 0.0 game-width game-height))
 
-
 (define (draw prev-time)
-  (draw-background)
-  (draw-image context image:ship
-              0.0 0.0 ship-width ship-height
-              (rect-x ship-hitbox) (rect-y ship-hitbox) ship-width ship-height)
+  (let ((ship (level-ship *level*)))
+    (draw-background)
+    (draw-image context image:ship
+                0.0 0.0 ship-width ship-height
+                (rect-x (ship-hitbox ship)) (rect-y (ship-hitbox ship)) ship-width ship-height))
   (request-animation-frame draw-callback))
 (define draw-callback (procedure->external draw))
 
