a4ca8b4f8454cbe4b2e26c0cd8b4c84b5c2218fd
Max Carvajal <jcarvajal288@gmail.com>
2024-05-18 11:41:44 -0500

    Blit ship in center of playfield

--- game.scm
+++ game.scm
@@ -15,13 +15,24 @@
         (math rect)
         (math vector))
 
-(define game-width    640.0)
-(define game-height   480.0)
+(define image:ship (make-image "assets/images/ship-1.png"))
+
+(define game-width    800.0)
+(define game-height   600.0)
+(define ship-width    86.0)
+(define ship-height   82.0)
+(define ship-hitbox (make-rect (- (/ game-width 2.0) (/ ship-width 2)) 
+                               (- (/ game-height 2.0) (/ ship-height 2))
+                               ship-width 
+                               ship-height))
 
 (define (draw prev-time)
     ;; Draw background
     (set-fill-color! context "#140c1c")
     (fill-rect context 0.0 0.0 game-width game-height)
+    (draw-image context image:ship
+                0.0 0.0 ship-width ship-height
+                (rect-x ship-hitbox) (rect-y ship-hitbox) ship-width ship-height)
   (request-animation-frame draw-callback))
 (define draw-callback (procedure->external draw))
 
