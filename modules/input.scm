569c876c0f16b8b2ea944f0043097b9d0c68f490
Max Carvajal <jcarvajal288@gmail.com>
2024-05-22 21:35:23 -0500

    Refactor update code

--- modules/input.scm
+++ modules/input.scm
@@ -8,10 +8,10 @@
   #:use-module (types)
   #:export (on-key-down
             on-key-up 
-            command:move-left
-            command:move-right
-            command:move-forward
-            command:move-backward
+            command:accelerate-left
+            command:accelerate-right
+            command:accelerate-forward
+            command:accelerate-backward
             command:rotate-left
             command:rotate-right))
 
@@ -20,10 +20,10 @@
 (define key:left "ArrowLeft")
 (define key:right "ArrowRight")
 
-(define command:move-left #f)
-(define command:move-right #f)
-(define command:move-forward #f)
-(define command:move-backward #f)
+(define command:accelerate-left #f)
+(define command:accelerate-right #f)
+(define command:accelerate-forward #f)
+(define command:accelerate-backward #f)
 (define command:rotate-left #f)
 (define command:rotate-right #f)
 
@@ -31,12 +31,12 @@
   (let ((key (keyboard-event-code event)))
     (cond ((string=? key key:left)  (set! command:rotate-left #t)) 
           ((string=? key key:right) (set! command:rotate-right #t)) 
-          ((string=? key key:up)    (set! command:move-forward #t))
-          ((string=? key key:down)  (set! command:move-backward #t)))))
+          ((string=? key key:up)    (set! command:accelerate-forward #t))
+          ((string=? key key:down)  (set! command:accelerate-backward #t)))))
 
 (define (on-key-up event)
   (let ((key (keyboard-event-code event)))
     (cond ((string=? key key:left)  (set! command:rotate-left #f)) 
           ((string=? key key:right) (set! command:rotate-right #f)) 
-          ((string=? key key:up)    (set! command:move-forward #f))
-          ((string=? key key:down)  (set! command:move-backward #f)))))
+          ((string=? key key:up)    (set! command:accelerate-forward #f))
+          ((string=? key key:down)  (set! command:accelerate-backward #f)))))
